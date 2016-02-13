3b1f0e714a56fa67f8ab54b2b78b6ee68de782a5
gra3 <gra3@zips.uakron.edu>
2016-02-12 23:07:46 -0500

    Added Weapon Implementation
    Wanted to try multiple derivations from a base class

--- OOP-HW2/Program.cs
+++ OOP-HW2/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_HW2
+namespace Oops.Game
 {
-    class Program
+    public class Player
     {
+        public object Weapon { get; set; }
+        public Armor Armor { get; set; }
+
+        Player()
+        {
+            Armor = new Pants();
+            Weapon = new Zweihander();
+        }
+
+        //Added so I can compile 
         static void Main(string[] args)
         {
+            return;
         }
     }
+
 }
